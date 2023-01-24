57ae2909a4b2288736cdd0dff2f48c2a00bf12a2
Mik7315 <kaplan.michael@hotmail.com>
2023-01-24 21:10:52 +0100

    Exercice 1

--- src/main/java/be/ephec/java/exercice/Main.java
+++ src/main/java/be/ephec/java/exercice/Main.java
@@ -1,7 +1,9 @@
 package be.ephec.java.exercice;
+import be.ephec.java.exercice.e01.Program;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Program prog = new Program();
+        prog.Exercice();
     }
 }
